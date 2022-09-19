27042dfd732ef69f97518396b40180fbd3143865
nachosanchezsolano <nacho.sanchezsolano@gmail.com>
2022-09-19 19:43:48 -0300

    commit the first exercise.

---
+++ first.py
@@ -0,0 +1,16 @@
+print("Este programa te genera la suma, resta, multiplicación y división de 2 números ingresados \n")
+
+a=int(input("Ingrese el primer número:"))
+
+b=int(input("\n Ingrese el segundo Número:"))
+
+print("\n La suma es:" + str((a+b)))
+
+print("\n La resta es: " + str((a-b)))
+
+print ("\n La multiplicación es: " + str((a*b)))
+
+print("\n La divición es: " + str((a/b)))
+
+
+
